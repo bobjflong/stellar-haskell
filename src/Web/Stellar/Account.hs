75f6e54bfa8822b8c86f1262021a600b3f9cf39b
Bob Long <robertjflong@gmail.com>
2014-11-09 22:00:04 -0800

    Account endpoint

--- src/Web/Stellar/Account.hs
+++ src/Web/Stellar/Account.hs
@@ -1,3 +1,105 @@
-module Web.Stellar.Account () where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
-hello = undefined
+module Web.Stellar.Account (
+    fetchAccount,
+    flags,
+    ownerCount,
+    previousTransactionID,
+    previousTxnLgrSeq,
+    stellarSequence,
+    stellarIndex,
+    balance,
+    MicroStellar(..),
+    Account(..)
+  ) where
+
+import           Control.Applicative
+import           Control.Lens        hiding ((.=))
+import           Data.Aeson
+import           Data.Text
+import           Debug.Trace
+import           Lens.Family
+import           Web.Stellar.Request
+
+-- | Provides a lens interface onto a Stellar account
+--
+-- >>> account ^. balance
+-- MicroStellar 6900888889
+--
+-- >>> account ^. flags
+-- 1048576
+--
+-- >>> account ^. ownerCount
+-- 0
+--
+-- >>> account ^. previousTransactionID
+-- "A584CEF24F24DAA16E99C2858B62A978975C448181FBF34B21B5873AFBA6A8AB
+--
+-- >>> account ^. previousTxnLgrSeq
+-- 1056876
+--
+-- >>> account ^. stellarSequence
+-- 4229
+--
+-- >>> account ^. stellarIndex
+-- "6047FB9C7976F2D0554618F5ABFF423E7136205BAF19E92BE9D295E549442C45"
+data Account = Account {
+  _balanceData           :: String,
+  _flags                 :: Int,
+  _ownerCount            :: Int,
+  _previousTransactionID :: Text,
+  _previousTxnLgrSeq     :: Int,
+  _stellarSequence       :: Int,
+  _stellarIndex          :: Text
+} deriving (Show)
+
+$(makeLenses ''Account)
+
+-- MicroStellars are the smallest divisible Stellar Unit
+data MicroStellar = MicroStellar Int | Unparseable deriving (Show)
+
+balance :: Lens' Account MicroStellar
+balance = lens getBalance setBalance
+  where getBalance acc = case (reads (_balanceData acc) :: [(Int, String)]) of
+                           [(a,"")] -> MicroStellar a
+                           _ -> Unparseable
+        setBalance a (MicroStellar x) = a { _balanceData = (show x) }
+        setBalance a _ = a { _balanceData = "unknown" }
+
+instance FromJSON Account where
+  parseJSON (Object v) = do
+    Account <$> v .: "Balance"
+    <*> v .: "Flags"
+    <*> v .: "OwnerCount"
+    <*> v .: "PreviousTxnID"
+    <*> v .: "PreviousTxnLgrSeq"
+    <*> v .: "Sequence"
+    <*> v .: "index"
+
+data AccountData = AccountData {
+  innerAccount :: Account
+} deriving (Show)
+
+instance FromJSON AccountData where
+  parseJSON (Object v) = do
+    AccountData <$> ((v .: "result") >>= (.: "account_data"))
+
+data AccountInfoRequest = AccountInfoRequest {
+  account :: Text
+}
+
+instance ToJSON AccountInfoRequest where
+  toJSON accountInfoRequest = object [
+    "method" .= ("account_info" :: Text),
+    "params" .= [object ["account" .= (account accountInfoRequest)]]]
+
+-- | Fetch account information for an Account ID
+--
+-- >>> r <- fetchAccount "https://test.stellar.org:9002" "abcdef..."
+fetchAccount :: StellarEndpoint -> Text -> IO (Maybe Account)
+fetchAccount endpoint accountId = do
+  accountData <- fetchTestAccountData
+  return $ fmap innerAccount accountData
+  where fetchTestAccountData :: IO (Maybe AccountData)
+        fetchTestAccountData = fmap decode $ makeRequest endpoint $ AccountInfoRequest accountId
--- src/Web/Stellar/Request.hs
+++ src/Web/Stellar/Request.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes        #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Web.Stellar.Request (
   PingResponse,
   StellarEndpoint,
   pingStellar,
-  defaultPingRequest
+  PingStatus(..),
+  makeRequest
 ) where
 
 import           Control.Applicative
@@ -15,9 +15,9 @@ import           Control.Lens         hiding ((.=))
 import           Data.Aeson
 import qualified Data.ByteString.Lazy as LBS
 import           Data.Text
+import           Debug.Trace
 import           GHC.Generics
 import           Network.Wreq
-import Debug.Trace
 
 requestOptions :: Options
 requestOptions = defaults & setContentType & setAccept
@@ -41,13 +41,27 @@ data PingResponse = PingResponse Text deriving (Show)
 instance FromJSON PingResponse where
   parseJSON (Object v) = PingResponse <$> ((v .: "result") >>= (.: "status"))
 
--- | Ping to test the connection to stellard 
+data PingStatus = PingSuccess | PingFailure deriving (Show)
+
+toPingStatus :: PingResponse -> PingStatus
+toPingStatus (PingResponse x) = if x == "success" then PingSuccess else PingFailure
+toPingStatus _ = PingFailure
+
+-- | Ping to test the connection to stellard
 --
 -- >>> :set -XOverloadedStrings
--- >>> pingStellar "https://test.stellar.org:9002" defaultPingRequest
--- Just (PingResponse "success") 
-pingStellar :: StellarEndpoint -> PingRequest -> IO (Maybe PingResponse)
-pingStellar endpoint ping = do
-  r <- post (unpack endpoint) (toJSON defaultPingRequest)
-  return $ decode (r ^. responseBody)
+-- >>> pingStellar "https://test.stellar.org:9002"
+-- Just PingSuccess
+pingStellar :: StellarEndpoint -> IO (Maybe PingStatus)
+pingStellar = flip pingStellar' defaultPingRequest
+  where pingStellar' :: StellarEndpoint -> PingRequest -> IO (Maybe PingStatus)
+        pingStellar' endpoint ping = do
+          r <- post (unpack endpoint) (toJSON defaultPingRequest)
+          return $ fmap toPingStatus $ decode (r ^. responseBody)
+
+makeRequest :: (ToJSON a) => StellarEndpoint -> a -> IO (LBS.ByteString)
+makeRequest x v = do
+  r <- post (unpack x) (toJSON v)
+  return $ r ^. responseBody
+
 
