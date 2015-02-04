24580faaa5e6df6efca0c66297039134bb4fd7c8
Bob Long <robertjflong@gmail.com>
2015-02-04 18:48:52 +0000

    updating to latest lens & wreq. Refactoring specs to hunit

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-runhaskell -isrc test/BasicProperties.hs
+runhaskell -isrc test/MoneyConversion.hs
 runhaskell -isrc test/Payment.hs
 runhaskell -isrc test/Submission.hs
 runhaskell -isrc test/OfferCreate.hs
---
+++ test/MoneyConversion.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
+
+module MoneyConversion where
+
+import           Control.Lens            hiding (elements)
+import           Data.Text
+import           Test.Hspec
+import           Test.QuickCheck
+import           Web.Stellar.AccountLine
+import           Web.Stellar.Types
+
+genNumericText :: Gen Text
+genNumericText = fmap pack $ listOf $ elements ['1'..'9']
+
+genSafeText :: Gen Text
+genSafeText = fmap pack $ listOf $ elements ['a'..'z']
+
+instance Arbitrary AccountLine where
+  arbitrary = do acc <- genSafeText
+                 bal <- genNumericText
+                 currency <- genSafeText
+                 limit <- genNumericText
+                 limitPeer <- genNumericText
+                 return $ AccountLine acc bal currency limit limitPeer
+
+main = hspec $ do
+  describe "MoneyLens laws" $ do
+    it "sets what it gets" $
+      property $ \v -> v == (balance .~ (v ^. balance) $ v)
+    it "gets back what it puts in" $
+      property $ \m a -> Just m == (balance .~ (Just m) $ a) ^. balance
+    it "sets twice safely" $
+      property $ \m a -> (balance .~ (Just m) $ a) == (balance .~ (Just m) $ (balance .~ (Just m) $ a))
