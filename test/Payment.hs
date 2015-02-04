2da20be29987df7790faf5f6f1ef05e534fb4c8c
Bob Long <robertjflong@gmail.com>
2015-02-04 20:34:19 +0000

    Refactoring Payment spec

--- test/Payment.hs
+++ test/Payment.hs
@@ -11,35 +11,57 @@ import           Data.Fixed
 import           Data.Maybe
 import           Data.Text
 import           Prelude             hiding (sequence)
+import           Test.Hspec
 import           Test.HUnit
 import           Web.Stellar.Payment
 import           Web.Stellar.Types
 
-payment1Encodes = TestCase (
-                    assertEqual
-                    "Basic payment params encoding"
-                    res
-                    (encode paymentParams1)
-                  )
-  where res = "{\"params\":[{\"secret\":\"2secret\",\"tx_json\":{\"TransactionType\":\"Payment\",\"Amount\":{\"value\":\"1\",\"currency\":\"USD\",\"issuer\":\"abc\"},\"Destination\":\"luckyduck\",\"Account\":\"moneybags\",\"Sequence\":4}}],\"method\":\"submit\"}"
-        amount1 = WithCurrency (CurrencyCode "USD") (Issuer "abc") 1
-        paymentParams1 = defaultPaymentParams & paymentAmount .~ amount1 &
-                                                secret .~ "2secret" &
-                                                fromAccount .~ "moneybags" &
-                                                toAccount .~ "luckyduck" &
-                                                sequence .~ 4
+main = hspec $ do
+  describe "Encoding" $ do
+    it "Correct encodes payment params" $
+      (encode paymentParams1) `shouldBe` paymentEncodeResult
+    it "Correctly decodes payment results" $
+      ((fromJust ((decode result1) :: Maybe SubmissionResponse)) ^. status) `shouldBe` SubmissionSuccess
+      
+paymentEncodeResult = "{\"params\":\
+                        \[{\"secret\":\"2secret\"\
+                         \,\"tx_json\":\
+                             \{\"TransactionType\":\"Payment\"\
+                             \,\"Amount\":\
+                                 \{\"value\":\"1\"\
+                                 \,\"currency\":\"USD\"\
+                                 \,\"issuer\":\"abc\"}\
+                                 \,\"Destination\":\"luckyduck\"\
+                                 \,\"Account\":\"moneybags\"\
+                                 \,\"Sequence\":4}\
+                          \}]\
+                       \,\"method\":\"submit\"}"
 
-result1Decodes = TestCase (
-                   assertEqual
-                   "Successful payment decoding"
-                   SubmissionSuccess
-                   ((fromJust ((decode result1) :: Maybe SubmissionResponse)) ^. status)
-                 )
-  where result1 = "{   \"result\" : {      \"engine_result\" : \"tesSUCCESS\",      \"engine_result_code\" : 0,      \"engine_result_message\" : \"The transaction was applied.\",      \"status\" : \"success\",      \"tx_blob\" : \"abc\",      \"tx_json\" : {         \"Account\" : \"abc\",         \"Amount\" : {            \"currency\" : \"USD\",            \"issuer\" : \"abc\",            \"value\" : \"1\"         },         \"Destination\" : \"abc\",         \"Fee\" : \"10\",         \"Flags\" : 2147483648,         \"Sequence\" : 2967,         \"SigningPubKey\" : \"abc\",         \"TransactionType\" : \"Payment\",         \"TxnSignature\" : \"abc\",         \"hash\" : \"abc\"      }   }}\r"
+amount1 = WithCurrency (CurrencyCode "USD") (Issuer "abc") 1
+paymentParams1 = defaultPaymentParams & paymentAmount .~ amount1 &
+                 secret .~ "2secret" &
+                 fromAccount .~ "moneybags" &
+                 toAccount .~ "luckyduck" &
+                 sequence .~ 4
 
-tests = TestList [
-          TestLabel "#1 encodes" payment1Encodes
-          , TestLabel "#1 decodes" result1Decodes
-        ]
-
-main = runTestTT tests
+result1 = "{\"result\":\
+            \{\"engine_result\":\"tesSUCCESS\"\
+            \,\"engine_result_code\":0\
+            \,\"engine_result_message\":\"The transaction was applied.\"\
+            \,\"status\":\"success\"\
+            \,\"tx_blob\":\"abc\"\
+            \,\"tx_json\":\
+              \{\"Account\":\"abc\"\
+              \,\"Amount\":\
+                \{\"currency\":\"USD\"\
+                \,\"issuer\":\"abc\"\
+                \,\"value\":\"1\"}\
+              \,\"Destination\":\"abc\"\
+              \,\"Fee\":\"10\"\
+              \,\"Flags\":2147483648\
+              \,\"Sequence\":2967\
+              \,\"SigningPubKey\":\"abc\"\
+              \,\"TransactionType\":\"Payment\"\
+              \,\"TxnSignature\":\"abc\"\
+              \,\"hash\":\"abc\"\
+            \}}}\r"
