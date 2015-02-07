84fa70c1063300195c4447aa2caf39d3ebe5e3ea
Bob Long <robertjflong@gmail.com>
2015-02-07 19:15:11 +0000

    Converting Submission tests to hspec

--- test/Submission.hs
+++ test/Submission.hs
@@ -2,19 +2,29 @@
 
 module Submission where
 
-import           Control.Applicative
 import           Control.Lens               hiding ((.=))
-import           Control.Monad              hiding (sequence)
-import           Data.Aeson
 import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BLC
-import           Data.Maybe
 import           Data.Maybe
 import           Data.Text
 import           Prelude                    hiding (sequence)
 import           System.IO.Unsafe
+import           Test.Hspec
 import           Test.HUnit
 import           Web.Stellar.Types
+
+main = hspec $ do
+  describe "engine errors" $ do
+    it "should report as an error" $ do
+      (sequenceError ^. status) `shouldBe` SubmissionError
+    it "should give a correct error message" $ do
+      (fromJust $ sequenceError ^. errorMessage) `shouldBe` "This sequence number has already past."
+  describe "stellar errors" $ do
+    it "should report as an error" $ do
+      (stellarError ^. status) `shouldBe` SubmissionError
+  describe "normal operation" $ do
+    it "should be ok" $ do
+      (submissionOK ^. status) `shouldBe` SubmissionSuccess
 
 sequenceError :: SubmissionResponse
 sequenceError = (fromJust.decode.BLC.pack) (unsafePerformIO $ readFile "test/fixtures/submission_sequence_error.json")
@@ -25,39 +35,3 @@ submissionOK = (fromJust.decode.BLC.pack) (unsafePerformIO $ readFile "test/fixt
 stellarError :: SubmissionResponse
 stellarError = (fromJust.decode.BLC.pack) (unsafePerformIO $ readFile "test/fixtures/stellar_error.json")
 
-sequenceErrorNotSuccess = TestCase (
-                            assertEqual
-                            "Engine errors should not be considered successful even though status = success in the json"
-                            SubmissionError
-                            (sequenceError ^. status)
-                          )
-
-sequenceErrorMessage = TestCase (
-                         assertEqual
-                         "Engine messages should show up in the errorMessage field"
-                         "This sequence number has already past."
-                         (fromJust $ sequenceError ^. errorMessage)
-                       )
-
-checkSuccess = TestCase (
-                 assertEqual
-                 "Error handling should not interfere with successes"
-                 SubmissionSuccess
-                 (submissionOK ^. status)
-               )
-
-checkStellarError = TestCase (
-                      assertEqual
-                      "Stellar errors should cause a SubmissionError"
-                      SubmissionError
-                      (stellarError ^. status)
-                    )
-
-tests = TestList [
-          TestLabel "sequenceError should not be successful" sequenceErrorNotSuccess,
-          TestLabel "engine messages should present as errorMessage" sequenceErrorMessage,
-          TestLabel "error handling should not interfere with successes" checkSuccess,
-          TestLabel "Stellar errors should cause a SubmissionError" checkStellarError
-        ]
-
-main = runTestTT tests
